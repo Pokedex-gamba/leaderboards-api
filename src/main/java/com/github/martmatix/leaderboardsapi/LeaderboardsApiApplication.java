10775488e7dd2fb8d3e440592369f298f419ceed
MartmatiX <martin.malir@gmail.com>
2024-11-16 17:53:39 +0100

    Initial commit

---
+++ src/main/java/com/github/martmatix/leaderboardsapi/LeaderboardsApiApplication.java
@@ -0,0 +1,13 @@
+package com.github.martmatix.leaderboardsapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeaderboardsApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LeaderboardsApiApplication.class, args);
+    }
+
+}
